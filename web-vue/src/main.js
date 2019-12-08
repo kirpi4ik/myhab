e602c457c3a897c82cef6ddbd91297216385390a
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2019-12-08 21:43:13 +0200

    apollo client added, first demo queries implemented

--- web-vue/src/main.js
+++ web-vue/src/main.js
@@ -2,22 +2,24 @@ import Vue from 'vue'
 import Vuelidate from 'vuelidate';
 
 import App from './App'
-import { router } from './_helpers';
+import {router} from './_helpers';
 
 import CoreuiVue from '@coreui/vue'
-import { iconsSet as icons } from './assets/icons/icons.js'
+import {iconsSet as icons} from './assets/icons/icons.js'
 
 Vue.config.performance = true
 Vue.use(CoreuiVue)
 Vue.use(Vuelidate);
 
+import apolloProvider from './graphql';
 
 new Vue({
-  el: '#app',
-  router,
-  icons,
-  template: '<App/>',
-  components: {
-    App
-  },
+    el: '#app',
+    router,
+    icons,
+    template: '<App/>',
+    components: {
+        App
+    },
+    apolloProvider
 })
