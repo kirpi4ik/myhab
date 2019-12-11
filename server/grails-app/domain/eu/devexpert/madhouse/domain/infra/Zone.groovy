8265accabd97f7885fa6612239b688805b8fbc1a
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2019-12-11 23:11:05 +0200

    fix navigation, for light/zones

--- server/grails-app/domain/eu/devexpert/madhouse/domain/infra/Zone.groovy
+++ server/grails-app/domain/eu/devexpert/madhouse/domain/infra/Zone.groovy
@@ -1,34 +1,73 @@
 package eu.devexpert.madhouse.domain.infra
 
+import eu.devexpert.madhouse.domain.common.BaseEntity
 import eu.devexpert.madhouse.domain.device.Cable
 import eu.devexpert.madhouse.domain.device.DevicePeripheral
-import eu.devexpert.madhouse.domain.common.BaseEntity
+import grails.gorm.DetachedCriteria
 import grails.rest.Resource
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import org.grails.gorm.graphql.entity.dsl.GraphQLMapping
+import org.grails.gorm.graphql.fetcher.impl.EntityDataFetcher
+import org.grails.gorm.graphql.fetcher.impl.SingleEntityDataFetcher
 
-@Resource(readOnly = false, formats = ['json', 'xml'])
+@Resource(readOnly = true, formats = ['json', 'xml'])
 class Zone extends BaseEntity {
-  String name
-  String description
-  Set<String> categories
-  Zone parent
-  Set<Zone> zones
-  Set<DevicePeripheral> peripherals
-  Set<Cable> cables
-  static belongsTo = [parent: Zone]
-  static hasOne = [parent: Zone]
-  static hasMany = [cables: Cable, peripherals: DevicePeripheral, zones: Zone]
+    String name
+    String description
+    Set<String> categories
+    Zone parent
+    Set<Zone> zones
+    Set<DevicePeripheral> peripherals
+    Set<Cable> cables
+    static belongsTo = [parent: Zone]
+    static hasOne = [parent: Zone]
+    static hasMany = [cables: Cable, peripherals: DevicePeripheral, zones: Zone]
 
 
-  static mapping = {
-    table '`zones`'
-    peripherals joinTable: [name: "zones_peripherals_join", key: 'zone_id']
-    cables joinTable: [name: "zones_cables_join", key: 'zone_id']
-    sort parent: "asc"
-  }
-  static constraints = {
-    peripherals cascade: 'save-update'
-    cables cascade: 'save-update'
-    zones cascade: 'save-update'
-  }
-  static graphql = true
+    static mapping = {
+        table '`zones`'
+        peripherals joinTable: [name: "zones_peripherals_join", key: 'zone_id']
+        cables joinTable: [name: "zones_cables_join", key: 'zone_id']
+        sort parent: "asc"
+    }
+    static constraints = {
+        peripherals cascade: 'save-update'
+        cables cascade: 'save-update'
+        zones cascade: 'save-update'
+    }
+    static graphql = GraphQLMapping.lazy {
+        query('zoneByUid', Zone) {
+            argument('uid', String)
+            dataFetcher(new DataFetcher() {
+                @Override
+                Object get(DataFetchingEnvironment environment) {
+                    Zone.findByUid(environment.getArgument('uid'))
+                }
+            })
+        }
+        query('zonesByParentUid', [Zone]) {
+            argument('uid', String)
+            dataFetcher(new EntityDataFetcher<Zone>(Zone.gormPersistentEntity) {
+                @Override
+                protected DetachedCriteria buildCriteria(DataFetchingEnvironment environment) {
+                    Zone.where {
+                        parent {
+                            eq('uid', environment.getArgument('uid') as String)
+                        }
+                    }
+                }
+            })
+        }
+        query('zonesRoot', [Zone]) {
+            dataFetcher(new EntityDataFetcher<Zone>(Zone.gormPersistentEntity) {
+                @Override
+                protected DetachedCriteria buildCriteria(DataFetchingEnvironment environment) {
+                    Zone.where { parent == null }
+
+                }
+            })
+        }
+    }
+
 }
