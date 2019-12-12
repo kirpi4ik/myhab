c3aa4d579c249898b87499cbe72754b35064baf1
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2019-12-12 23:47:15 +0200

    Add permission for top navigation

--- web-vue/src/App.vue
+++ web-vue/src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <script>
-  import { authenticationService } from '@/_services';
-  import { router, Role } from '@/_helpers';
+    import {authenticationService} from '@/_services';
+    import {router, Role} from '@/_helpers';
 
-  export default {
-    name: 'App',
-    data () {
-      return {
-        currentUser: null
-      };
-    },
-    computed: {
-      isAdmin () {
-        return this.currentUser && this.currentUser.role === Role.Admin;
-      }
-    },
-    created () {
-      authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    }
-  };
+    export default {
+        name: 'App',
+        data() {
+            return {
+                currentUser: null,
+                title: process.env.VUE_APP_TITLE
+            };
+        },
+        computed: {
+            isAdmin() {
+                return this.currentUser && this.currentUser.role === Role.Admin;
+            }
+        },
+        created() {
+            authenticationService.currentUser.subscribe(x => this.currentUser = x);
+        }
+    };
 </script>
 
 <style lang="scss">
-  // Import Main styles for this application
-  @import 'assets/scss/style';
+    // Import Main styles for this application
+    @import 'assets/scss/style';
 </style>
