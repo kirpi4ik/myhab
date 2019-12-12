4c4c60626e3f423ab30b5a24ff3be60c3de03fe7
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2019-12-12 14:13:12 +0200

    Add authorization for navigation left display

--- web-vue/src/containers/TheSidebar.vue
+++ web-vue/src/containers/TheSidebar.vue
@@ -1,43 +1,61 @@
 <template>
-  <CSidebar 
-    fixed 
-    :minimize="minimize"
-    :show.sync="show"
-  >
-    <CSidebarBrand 
-      :imgFull="{ width: 118, height: 46, alt: 'Logo', src: 'img/brand/coreui-base-white.svg'}"
-      :imgMinimized="{ width: 118, height: 46, alt: 'Logo', src: 'img/brand/coreui-signet-white.svg'}"
-      :wrappedInLink="{ href: 'https://coreui.io/', target: '_blank'}"
-    />
-    <CRenderFunction flat :content-to-render="nav"/>
-    <CSidebarMinimizer
-      class="d-md-down-none"
-      @click.native="minimize = !minimize"
-    />
-  </CSidebar>
+    <CSidebar
+            fixed
+            :minimize="minimize"
+            :show.sync="show"
+    >
+        <CSidebarBrand
+                :imgFull="{ width: 118, height: 46, alt: 'Logo', src: 'img/brand/coreui-base-white.svg'}"
+                :imgMinimized="{ width: 118, height: 46, alt: 'Logo', src: 'img/brand/coreui-signet-white.svg'}"
+                :wrappedInLink="{ href: 'https://coreui.io/', target: '_blank'}"
+        />
+        <CRenderFunction flat :content-to-render="navItems"/>
+        <CSidebarMinimizer
+                class="d-md-down-none"
+                @click.native="minimize = !minimize"
+        />
+    </CSidebar>
 </template>
 
 <script>
-import nav from './_nav'
+    import {NAV} from './_nav'
+    import {authenticationService} from '@/_services';
+    import {Role} from '@/_helpers';
 
-export default {
-  name: 'TheSidebar',
-  data () {
-    return {
-      minimize: false,
-      nav,
-      show: 'responsive'
+
+    export default {
+        name: 'TheSidebar',
+        data() {
+            return {
+                minimize: false,
+                navItems: [],
+                show: 'responsive'
+            }
+        },
+        mounted() {
+            const currentUser = authenticationService.currentUserValue;
+            let filterNav = function (navItem) {
+                let hasRole = true;
+                if (navItem.roles != null) {
+                    hasRole = currentUser.permissions.filter(function (userRole) {
+                        return navItem.roles.includes(userRole);
+                    }).length > 0;
+
+                }
+                if (hasRole && navItem._children) {
+                    navItem._children = navItem._children.filter(filterNav)
+                }
+                return hasRole;
+            };
+            this.navItems = NAV.filter(filterNav);
+            this.$root.$on('toggle-sidebar', () => {
+                const sidebarOpened = this.show === true || this.show === 'responsive'
+                this.show = sidebarOpened ? false : 'responsive'
+            })
+            this.$root.$on('toggle-sidebar-mobile', () => {
+                const sidebarClosed = this.show === 'responsive' || this.show === false
+                this.show = sidebarClosed ? true : 'responsive'
+            })
+        }
     }
-  },
-  mounted () {
-    this.$root.$on('toggle-sidebar', () => {
-      const sidebarOpened = this.show === true || this.show === 'responsive'
-      this.show = sidebarOpened ? false : 'responsive'
-    })
-    this.$root.$on('toggle-sidebar-mobile', () => {
-      const sidebarClosed = this.show === 'responsive' || this.show === false
-      this.show = sidebarClosed ? true : 'responsive'
-    })
-  }
-}
 </script>
--- web-vue/src/containers/_nav.js
+++ web-vue/src/containers/_nav.js
@@ -1,4 +1,6 @@
-export default [
+import {Role} from '@/_helpers';
+
+export const NAV = [
     {
         _name: 'CSidebarNav',
         _children: [
@@ -14,40 +16,48 @@ export default [
             },
             {
                 _name: 'CSidebarNavTitle',
-                _children: ['Control']
+                _children: ['Control'],
+                roles: [Role.Super]
             },
             {
                 _name: 'CSidebarNavItem',
                 name: 'Lumina',
                 to: '/theme/colors',
-                icon: 'cil-drop'
+                icon: 'cil-drop',
+                roles: [Role.Super]
             },
             {
                 _name: 'CSidebarNavItem',
                 name: 'Incalzire',
                 to: '/theme/typography',
-                icon: 'cil-pencil'
+                icon: 'cil-pencil',
+                roles: [Role.Super]
             },
             {
                 _name: 'CSidebarNavItem',
                 name: 'Temperatura',
                 to: '/theme/typography',
-                icon: 'cil-pencil'
+                icon: 'cil-pencil',
+                roles: [Role.Super]
             },
             {
                 _name: 'CSidebarNavTitle',
-                _children: ['Setari']
+                _children: ['Setari'],
+                roles: [Role.Super]
             },
             {
                 _name: 'CSidebarNavDropdown',
                 name: 'Utilizatori',
                 route: '/users',
                 icon: 'cil-puzzle',
+                roles: [Role.Super],
                 items: [
                     {
                         name: 'Lista utilizatorilor',
                         to: '/users',
-                        icon: 'cil-puzzle'
+                        icon: 'cil-puzzle',
+                        roles: [Role.Super]
+
                     }
                 ]
             }
