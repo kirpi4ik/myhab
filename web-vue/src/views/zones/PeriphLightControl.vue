4640a2be03b2d8043d519b6c3ee76e844685c7ad
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2019-12-13 12:38:02 +0200

    Add disaply for temp and heat control, split into components peripheral

---
+++ web-vue/src/views/zones/PeriphLightControl.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="card" :class="`card-background text-white`">
+        <div class="card-body pb-2">
+            <slot></slot>
+            <div>
+                <font-awesome-icon icon="lightbulb" size="3x" :class="`zone-icon-${peripheral.state}`"/>
+                <h4 class="mb-1"> {{peripheral.data.name}}</h4>
+            </div>
+
+        </div>
+        <slot name="footer" class="card-footer">
+            <div class="toggle-btn">
+                <toggle-button v-model="peripheral.state" color="#82C7EB" :sync="true"
+                               :labels="{checked: 'Aprinde', unchecked: 'Stinge'}"
+                               :switch-color="{checked: 'linear-gradient( #8DFF73, green)', unchecked: 'linear-gradient(#BF0000, #FFBE62)'}"
+                               :color="{checked: '#009663', unchecked: '#FF0000', disabled: '#CCCCCC'}"
+                               :speed="300"
+                               @change="periphStateChangeHandler(peripheral)"
+                               :font-size="14"
+                               :width="250"
+                               :height="40"/>
+            </div>
+        </slot>
+    </div>
+</template>
+
+<script>
+    import {router} from '@/_helpers';
+    import {authenticationService} from '@/_services';
+    import { PUSH_EVENT} from "../../graphql/zones";
+
+    export default {
+        name: 'PeriphLightControl',
+        props: {
+            peripheral: Object
+        },
+        methods: {
+            periphStateChangeHandler: function (peripheral) {
+                let event = {
+                    "p0": "light",
+                    "p1": "PERIPHERAL",
+                    "p2": peripheral.data.uid,
+                    "p3": "mweb",
+                    "p4": peripheral.state === true ? "off" : "on",
+                    "p6": authenticationService.currentUserValue.login
+                };
+                this.$apollo.mutate({
+                    mutation: PUSH_EVENT, variables: {input: event}
+                }).then(response => {
+
+                });
+            }
+        }
+    }
+</script>
+<style scoped>
+    .zone-card-background {
+        background-image: linear-gradient(#4f6167, #8e949f);
+    }
+
+    .card-background {
+        background-image: linear-gradient(#156cb8, #60a3c2);
+    }
+
+    .card-footer {
+        text-align: center;
+    }
+
+    .zone-card-footer {
+        height: 50pt;
+    }
+
+    .toggle-btn {
+        margin: auto;
+    }
+
+    div.v-switch-core {
+        border: 1px solid #000000 !important;
+    }
+
+    .zone-icon-false {
+        float: right;
+        color: yellow;
+    }
+
+    .zone-icon-true {
+        float: right;
+        fill: #044B9466;
+        fill-opacity: 0.5;
+    }
+</style>
