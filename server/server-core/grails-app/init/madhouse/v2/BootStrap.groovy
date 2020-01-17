21af5ade21fbcd841ca0335dcebcc8f42ffa9021
kirpi4ik <dumitru.ciubenco@gmail.com>
2020-01-17 18:04:16 +0200

    Add opsgiene alert service

--- server/server-core/grails-app/init/madhouse/v2/BootStrap.groovy
+++ server/server-core/grails-app/init/madhouse/v2/BootStrap.groovy
@@ -1,10 +1,16 @@
 package madhouse.v2
 
+import eu.devexpert.madhouse.alert.Alert
+import eu.devexpert.madhouse.alert.AlertPriority
+
 class BootStrap {
+    def alertService
 
     def schedulerService
     def init = { servletContext ->
 //        schedulerService.startAll()
+//        def var = new Alert(AlertPriority.P1, "helo", "description")
+//        alertService.sendAlert(var)
     }
     def destroy = {
 //        schedulerService.shutdown()
