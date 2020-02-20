8168facc7bce7e6ccf2e5f97ffcf5ac27bb6cb33
kirpi4ik <dumitru.ciubenco@gmail.com>
2020-02-20 14:47:25 +0200

    Heat contorl use celsius instead of time
    Former-commit-id: 6ba5f78ae91d9352042e287404c36ec1d03745b7

--- server/server-core/grails-app/jobs/eu/devexpert/madhouse/jobs/HeatingControlJob.groovy
+++ server/server-core/grails-app/jobs/eu/devexpert/madhouse/jobs/HeatingControlJob.groovy
@@ -1,18 +1,60 @@
 package eu.devexpert.madhouse.jobs
 
+import eu.devexpert.madhouse.domain.device.DevicePeripheral
+import eu.devexpert.madhouse.domain.infra.Zone
 import grails.events.EventPublisher
 import grails.gorm.transactions.Transactional
+import groovy.util.logging.Slf4j
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.quartz.JobExecutionException
 
+import java.util.concurrent.TimeUnit
+
 /**
  *
  */
+@Slf4j
 @Transactional
 class HeatingControlJob implements Job, EventPublisher {
-  @Override
-  void execute(JobExecutionContext context) throws JobExecutionException {
+    static triggers = {
+        simple name: 'heatControlJob', repeatInterval: TimeUnit.MINUTES.toMillis(20)
+    }
+    def heatService
+    def tempAllDay = 20
 
-  }
+    static group = "Internal"
+    static description = "Heat control"
+
+    @Override
+    void execute(JobExecutionContext context) throws JobExecutionException {
+        Set<DevicePeripheral> peripheralsWithHeatCircuit = DevicePeripheral.where {
+            category.name == "HEAT"
+        }.findAll()
+
+        peripheralsWithHeatCircuit.each { peripheral ->
+            def tempValue = Double.valueOf(peripheral.configurations.find { config -> config.key = "key.temp.allDay.value" }?.value ?: tempAllDay)
+            Set<Zone> leafZones = peripheral.zones.findAll { zone -> zone.zones.empty }
+            leafZones.each { leafZone ->
+                DevicePeripheral.createCriteria().list {
+                    category {
+                        eq('name', 'TEMP')
+                    }
+                    zones {
+                        idEq(leafZone.id)
+                    }
+                }.each { termostat ->
+                    termostat.connectedTo.findAll { termostatPort -> termostatPort.value?.isNumber() }.each { termostatPort ->
+                        if (Double.valueOf(termostatPort.value) <= tempValue) {
+                            println("[${termostatPort.value} < ${tempValue}] | HEAT ON: ${leafZone.name}")
+                            heatService.heatOn(peripheral)
+                        } else {
+                            println("[${termostatPort.value} > ${tempValue}] | HEAT OFF: ${leafZone.name}")
+                            heatService.heatOff(peripheral)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
