147d37b5dda371154104fca8caa9729a6637b388
kirpi4ik <dumitru.ciubenco@gmail.com>
2020-03-02 12:06:52 +0200

    2.0.5-SNAPSHOT
    Former-commit-id: bc4d03ec4c27df6cf15e6dd8c39a79885b22b0b6

---
+++ web-vue/src/views/dashboard/TempChartControl.vue
@@ -0,0 +1,36 @@
+<template>
+    <CCard>
+        <CCardBody>
+            <CRow>
+                <iframe :src="grafanaUrl+'/d-solo/a588yoxWz/temperatura?orgId=1&theme=light&panelId=4'"
+                        width="100%" height="400" frameborder="0"></iframe>
+            </CRow>
+        </CCardBody>
+    </CCard>
+</template>
+<script>
+    import {CChartLine} from '@coreui/vue-chartjs'
+
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    export default {
+        name: 'TempChartControl',
+        components: {
+            CChartLine
+        },
+        data() {
+            return {
+                grafanaUrl: process.env.VUE_APP_CONF_GRAFANA_URL
+            }
+        }
+    }
+</script>
+<style>
+    .dashboard-header {
+        color: #4d80e0;
+
+
+    }
+</style>
