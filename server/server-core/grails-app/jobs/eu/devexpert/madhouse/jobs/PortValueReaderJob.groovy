0add8a951e61eee5d63d85890ea160985055c31f
kirpi4ik <dumitru.ciubenco@gmail.com>
2020-10-31 17:26:41 +0200

    Add status to the Device, update status when read port value fails/succeed Upgrade grails 4.0.5
    Former-commit-id: 082800a62fd3aa72749910fccd2e4498e318e0ee

--- server/server-core/grails-app/jobs/eu/devexpert/madhouse/jobs/PortValueReaderJob.groovy
+++ server/server-core/grails-app/jobs/eu/devexpert/madhouse/jobs/PortValueReaderJob.groovy
@@ -1,7 +1,11 @@
 package eu.devexpert.madhouse.jobs
 
+import eu.devexpert.madhouse.domain.EntityType
+import eu.devexpert.madhouse.domain.TopicName
 import eu.devexpert.madhouse.domain.device.Device
 import eu.devexpert.madhouse.domain.device.DeviceModel
+import eu.devexpert.madhouse.domain.device.DeviceStatus
+import eu.devexpert.madhouse.exceptions.UnavailableDeviceException
 import eu.devexpert.madhouse.services.EspDeviceService
 import eu.devexpert.madhouse.services.MegaDriverService
 import eu.devexpert.madhouse.services.PortValueService
@@ -38,16 +42,56 @@ class PortValueReaderJob implements Job, EventPublisher {
                         megaDriverService.readPortValues(deviceUid)
                     }.onComplete { portValues ->
                         portValueService.updatePortValues(deviceUid, portValues)
+                        publish(TopicName.EVT_DEVICE_STATUS.id(), [
+                                "p0": TopicName.EVT_DEVICE_STATUS.id(),
+                                "p1": EntityType.DEVICE.name(),
+                                "p2": installedDevice?.uid,
+                                "p3": "read_device_controller_status",
+                                "p4": DeviceStatus.ONLINE,
+                                "p6": "system"
+                        ])
+                    }.onError { Throwable t ->
+                        log.error("onError reading port value : deviceUid=${installedDevice.uid}")
                     }
                 } else if (installedDevice.model.equals(DeviceModel.ESP8266_1)) {
                     Promises.task {
-                        espDeviceService.readPortValues(deviceUid)
+                        try {
+                            espDeviceService.readPortValues(deviceUid)
+                        } catch (UnavailableDeviceException ex) {
+                            publish(TopicName.EVT_DEVICE_STATUS.id(), [
+                                    "p0": TopicName.EVT_DEVICE_STATUS.id(),
+                                    "p1": EntityType.DEVICE.name(),
+                                    "p2": installedDevice?.uid,
+                                    "p3": "read_device_controller_status",
+                                    "p4": DeviceStatus.OFFLINE,
+                                    "p6": "system"
+                            ])
+                            throw new UnavailableDeviceException()
+                        }
                     }.onComplete { portValues ->
                         portValueService.updatePortValues(deviceUid, portValues)
+                        publish(TopicName.EVT_DEVICE_STATUS.id(), [
+                                "p0": TopicName.EVT_DEVICE_STATUS.id(),
+                                "p1": EntityType.DEVICE.name(),
+                                "p2": installedDevice?.uid,
+                                "p3": "read_device_controller_status",
+                                "p4": DeviceStatus.ONLINE,
+                                "p6": "system"
+                        ])
+                    }.onError { Throwable t ->
+                        log.error("onError reading port value : deviceUid=${installedDevice.uid}")
                     }
                 }
 
-            } catch (Exception ex) {
+            } catch (UnavailableDeviceException ex) {
+                publish(TopicName.EVT_DEVICE_STATUS.id(), [
+                        "p0": TopicName.EVT_DEVICE_STATUS.id(),
+                        "p1": EntityType.DEVICE.name(),
+                        "p2": installedDevice?.uid,
+                        "p3": "read_device_controller_status",
+                        "p4": DeviceStatus.OFFLINE,
+                        "p6": "system"
+                ])
                 log.error("Error reading port value : deviceUid=${installedDevice.uid}", ex)
             }
         }
