aaf5b6a7263ea0c83e9ba8d3108daa07d313fecc
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2021-03-10 18:18:05 +0200

    Websocket 2 (#14)
    * small fixes
    
    * add public event log
    use partitioned port_values
    Add backup entity - still in work
    
    * add websockets lib
    
    * serverside websocets working
    
    * STOMP/Websockets  - feature added
    only evt_light event supported
    
    * support websocket reconnect
    
    * move stompjs to vuex
    refresh view when server side is changed
    display online-offline status
    
    * delay event sending to avoid UI spam
    
    * disable event logging
    
    * increase version

---
+++ web-vue/src/_helpers/vuex.js
@@ -0,0 +1,85 @@
+import {Stomp, Client, ActivationState} from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+import {authenticationService} from '@/_services';
+import Vue from 'vue'
+import Vuex from "vuex";
+
+
+Vue.use(Vuex);
+
+let stompClient = null;
+const state = {
+    stomp: {
+        message: null,
+        connection: 'OFFLINE'
+    },
+    config: {
+        grafanaUrl: "https://grafana.madhouse.app"
+    }
+};
+const mutations = {
+    init(state) {
+
+    },
+    stompValue(state, newValue) {
+        state.stomp.message = newValue;
+
+    },
+    stompConnection(state, newValue) {
+        state.stomp.connection = newValue;
+
+    }
+};
+const actions = {
+    connect(context, handler) {
+        const wsUri = process.env.VUE_APP_SERVER_URL + '/stomp?access_token=' + authenticationService.currentUserValue.access_token;
+        console.log('STOMP: Attempting connection');
+
+        let message_callback = function (message) {
+            if (message.headers['content-type'] === 'application/octet-stream') {
+                this.commit("stompValue", message.binaryBody);
+            } else {
+                this.commit("stompValue", JSON.parse(message.body));
+            }
+        }.bind(this);
+
+        this.stompClient = new Client({
+            brokerURL: wsUri,
+            debug: function (str) {
+                console.log(str);
+            },
+            heartbeatIncoming: 4000,
+            heartbeatOutgoing: 4000,
+            webSocketFactory: function () {
+                return new SockJS(wsUri);
+            },
+            onConnect: () => {
+                console.log("CONNECTED");
+                this.commit("stompConnection", 'ONLINE');
+                this.stompClient.subscribe("/topic/events", message_callback, {});
+                if (handler != null) {
+                    this.stompClient.subscribe("/topic/events", handler, {});
+                }
+            },
+            onStompError: this.stompFailureCallback,
+            onWebSocketError: this.stompFailureCallback
+        });
+        console.log('Connecting...');
+        this.stompClient.activate();
+    },
+    stompFailureCallback(error) {
+        this.commit("stompConnection", 'OFFLINE');
+        console.log('STOMP error: ' + error);
+        setTimeout(this.stompConnect, 5000);
+        console.log('STOMP: Reconecting in 5 seconds');
+    }
+};
+const store = new Vuex.Store({
+        state,
+        mutations,
+        actions
+    }
+);
+export const vuex = {
+    store
+};
