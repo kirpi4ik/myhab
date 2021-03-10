f30b639440baa0368c402ddf2ad1d8260030f9b8
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2021-03-10 18:18:05 +0200

    Websocket 2 (#14)
    * small fixes
    
    * add public event log
    use partitioned port_values
    Add backup entity - still in work
    
    * add websockets lib
    
    * serverside websocets working
    
    * STOMP/Websockets  - feature added
    only evt_light event supported
    
    * support websocket reconnect
    
    * move stompjs to vuex
    refresh view when server side is changed
    display online-offline status
    
    * delay event sending to avoid UI spam
    
    * disable event logging
    
    * increase version

--- server/server-core/grails-app/services/eu/devexpert/madhouse/services/WSocketsService.groovy
+++ server/server-core/grails-app/services/eu/devexpert/madhouse/services/WSocketsService.groovy
@@ -5,13 +5,35 @@ import grails.events.EventPublisher
 import grails.events.annotation.Subscriber
 import grails.gorm.transactions.Transactional
 import grails.plugin.springwebsocket.WebSocket
+import org.joda.time.DateTime
 
 @Transactional
 class WSocketsService implements EventPublisher, WebSocket {
 
+    int periodMs = 3000
+    long lastEvent = DateTime.now().millis
+
     @Subscriber('evt_light')
-    def hello() {
+    def evtLight() {
 //        convertAndSendToUser("admin", "/queue/hello", "hello, target user!")
         convertAndSend("/topic/events", new WSocketEvent(eventName: "evt_light"))
+    }
+
+    @Subscriber('evt_port_value_changed')
+    def evtPortChanged() {
+        if (DateTime.now().millis - lastEvent > periodMs) {
+            convertAndSend("/topic/events", new WSocketEvent(eventName: "evt_port_value_changed"))
+            lastEvent = DateTime.now().millis
+        }
+    }
+
+    @Subscriber('evt_heat')
+    def evtHeat() {
+        convertAndSend("/topic/events", new WSocketEvent(eventName: "evt_heat"))
+    }
+
+    @Subscriber('evt_device_status')
+    def evtReadDeviceStatus() {
+        convertAndSend("/topic/events", new WSocketEvent(eventName: "evt_device_status"))
     }
 }
