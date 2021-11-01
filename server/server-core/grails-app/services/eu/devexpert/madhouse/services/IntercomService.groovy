11192527f56b175f524c6349c947025e7e9a8062
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2021-11-01 16:37:35 +0200

    2.2.14 (#42)
    * 2.2.14
    
    * 2.2.14 - Replace doorbell by mega-d controller to control lock, enable heat control

--- server/server-core/grails-app/services/eu/devexpert/madhouse/services/IntercomService.groovy
+++ server/server-core/grails-app/services/eu/devexpert/madhouse/services/IntercomService.groovy
@@ -1,6 +1,8 @@
 package eu.devexpert.madhouse.services
 
 import eu.devexpert.madhouse.domain.device.Device
+import eu.devexpert.madhouse.domain.device.port.DevicePort
+import eu.devexpert.madhouse.domain.device.port.PortAction
 import eu.devexpert.madhouse.exceptions.PeripheralActionException
 import eu.devexpert.madhouse.exceptions.UnavailableDeviceException
 import eu.devexpert.madhouse.utils.DeviceHttpService
@@ -12,20 +14,14 @@ import org.joda.time.DateTime
 @Slf4j
 class IntercomService {
 
-    def doorOpen(deviceId) throws UnavailableDeviceException {
+    def doorOpen(deviceId, DevicePort port) throws UnavailableDeviceException {
         Device device = Device.findById(deviceId)
         try {
-            def doorconnect = new DeviceHttpService(device: device, uri: "cgi-bin/hi3510/doorConnect.cgi?&-door=0").writeState()
-        } catch (SocketException se) {
-            //Unexpected end of file from server
-            log.trace("Unexpected end of file from server [${se.message}]")
-            try {
-                def doorunlock = new DeviceHttpService(device: device, uri: "web/cgi-bin/hi3510/doorUnlock.cgi?&-time=${DateTime.now().millis}").writeState()
-                def resp = doorunlock?.text()
-                log.debug("Door unlock")
-            } catch (Exception ex) {
-                throw new PeripheralActionException(ex.message)
-            }
+            def doorunlock = new DeviceHttpService(device: device, port: port, actions: [PortAction.ON, "p7", PortAction.OFF]).writeState()
+            def resp = doorunlock?.text()
+            log.debug("Door unlock")
+        } catch (Exception ex) {
+            throw new PeripheralActionException(ex.message)
         }
     }
 
