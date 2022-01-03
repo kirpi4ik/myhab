593a7daaebfcdd06dcb08999678fce872c74b2c4
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2022-01-03 16:23:17 +0200

    2.3.6 (#68)
    * remove unused jpg
    add mobile icon
    
    * add telegram bot integration
    
    * add telegram bot integration
    
    * remove mock init from InitService
    
    * migrate to jdk 11
    add git based configuration provider
    move part of te config to GIT

--- server/server-core/grails-app/init/eu/devexpert/madhouse/init/BootStrap.groovy
+++ server/server-core/grails-app/init/eu/devexpert/madhouse/init/BootStrap.groovy
@@ -1,17 +1,11 @@
 package eu.devexpert.madhouse.init
 
-import eu.devexpert.rules.facts.HeatControlInside
-import org.jeasy.rules.api.Facts
-import org.jeasy.rules.api.Rules
-
 class BootStrap {
     def telegramBotHandler
     def init = { servletContext ->
         telegramBotHandler.sendMessage("INFO", "\uD83D\uDE80 Salut! sistemul Madhouse tocmai a pornit")
-
     }
     def destroy = {
-//        schedulerService.shutdown()
     }
 
 }
