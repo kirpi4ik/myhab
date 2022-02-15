9aa875379d641f7a8c961faa4bee6cc4e5c07e60
kirpi4ik <dumitru.ciubenco@gmail.com>
2022-02-15 20:50:04 +0200

    configure breadcrumb for vue3

--- client/web-vue3/src/boot/i18n.js
+++ client/web-vue3/src/boot/i18n.js
@@ -4,7 +4,7 @@ import { createI18n } from 'vue-i18n';
 const i18n = createI18n({
 	locale: process.env.VUE_APP_I18N_LOCALE || 'en',
 	fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-	messages: loadLocaleMessages(),
+	messages: loadLocaleMessages()
 });
 
 function loadLocaleMessages() {
