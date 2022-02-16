b8bf62e2a83122b89d5dbbee947f6b3cf3701290
kirpi4ik <dumitru.ciubenco@gmail.com>
2022-02-16 21:03:26 +0200

    Add breadcrumb for zone navigation

---
+++ client/web-vue3/src/components/cards/PeripheralLightCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-card class="text-white" style="background: linear-gradient(#156cb8, #60a3c2)">
+    <q-item>
+      <q-item-section avatar>
+        <q-avatar size="60px" :class="asset['state']? 'shadow-10 bg-blue' : 'shadow-10 bg-yellow-4'">
+          <q-icon name="mdi-lightbulb-on" color="yellow-10" size="40px" v-if="!asset['state']"/>
+          <q-icon name="mdi-lightbulb-off" color="white" size="40px" v-if="asset['state']"/>
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label class="text-weight-medium text-h5">{{ asset.data.name }}</q-item-label>
+      </q-item-section>
+
+      <q-item-section side>
+        <q-item-label>
+          <q-btn size="sm" flat round icon="settings" class="text-white"/>
+        </q-item-label>
+        <q-item-label>
+          <q-btn size="sm" flat round icon="mdi-view-list" class="text-white"/>
+        </q-item-label>
+
+      </q-item-section>
+    </q-item>
+
+    <q-separator></q-separator>
+    <q-card-section>
+      <div class="q-pa-sm text-grey-8">
+        <toggle v-model="asset['state']"/>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+<script>
+  import {defineComponent, toRefs} from 'vue';
+  import Toggle from '@vueform/toggle'
+
+  export default defineComponent({
+    name: 'PeripheralLightCard',
+    components: {
+      Toggle
+    },
+    props: {
+      peripheral: Object
+    },
+    setup(props) {
+      const {peripheral: asset} = toRefs(props)
+      return {asset};
+    },
+    methods: {},
+  });
+</script>
+<style>
+  .toggle, .toggle-container {
+    width: 100% !important;
+    height: 40px !important;
+  }
+
+  .toggle-handle {
+    height: 40px !important;
+    width: 40px !important;
+  }
+
+  .toggle-off {
+    background-color: red !important;
+  }
+
+  .toggle-on .toggle-handle {
+    background: linear-gradient(rgb(0, 191, 54), rgb(53, 113, 61)) !important;
+    border: 1px solid #386668;
+  }
+
+  .toggle-off .toggle-handle {
+    background: linear-gradient(rgb(191, 0, 0), rgb(255, 190, 98)) !important;
+    border: 1px solid #386668;
+  }
+</style>
+<style src="@vueform/toggle/themes/default.css"></style>
--- client/web-vue3/src/layouts/components/BreadCrumbLayout.vue
+++ client/web-vue3/src/layouts/components/BreadCrumbLayout.vue
@@ -1,35 +1,36 @@
 <template>
-	<q-separator class="bg-blue-grey-2" />
-	<q-breadcrumbs class="text-blue-grey bg-blue-grey-1" style="padding-top: 2px; padding-left: 5px">
-		<template v-slot:separator>
-			<q-icon size="1.5em" name="chevron_right" color="primary" />
-		</template>
-		<q-breadcrumbs-el :label="$t('navigation.home')" icon="home" to="/" />
-		<q-breadcrumbs-el :label="path.name" icon="widgets" :to="path.path" v-for="path in paths" v-bind:key="path" />
-	</q-breadcrumbs>
+  <q-separator class="bg-blue-grey-2"/>
+  <q-breadcrumbs class="text-blue-grey bg-blue-grey-1" style="padding-top: 2px; padding-left: 5px" v-if="breadcrumb && breadcrumb.navigation">
+    <template v-slot:separator>
+      <q-icon size="1.5em" name="chevron_right" color="primary"/>
+    </template>
+    <q-breadcrumbs-el :label="$t('navigation.home')" icon="home" to="/"/>
+    <q-breadcrumbs-el :label="link.name" icon="widgets" :to="'/zones/'+link.zoneId+'?category='+route.query.category" v-for="link in breadcrumb.navigation.breadcrumb" v-bind:key="link.zoneId"/>
+  </q-breadcrumbs>
 </template>
 <script>
-import { defineComponent } from 'vue';
+  import {defineComponent, ref} from 'vue';
+  import {useQuery} from "@vue/apollo-composable";
+  import {NAV_BREADCRUMB} from "@/graphql/queries";
+  import {useRoute} from "vue-router";
 
-export default defineComponent({
-	name: 'BreadCrumbLayout',
-	components: {},
-	data() {
-		return {
-			paths: [],
-		};
-	},
-	mounted() {
-		this.init();
-	},
-	watch: {
-		$route() {
-			this.paths = this.$route.meta.breadcrumb;
-		},
-	},
-	methods: {
-		init() {},
-		onResize() {},
-	},
-});
+  export default defineComponent({
+    name: 'BreadCrumbLayout',
+    components: {},
+    setup() {
+      const route = useRoute();
+
+      const {result: breadcrumb} = useQuery(NAV_BREADCRUMB, {zoneId: route.params.zoneId}, {
+        fetchPolicy: "network-only",   // Used for first execution
+        nextFetchPolicy: "cache-and-network" // Used for subsequent executions
+      })
+      console.log(breadcrumb)
+      return {route, breadcrumb}
+    },
+    watch: {
+      $route() {
+        this.paths = this.$route.meta.breadcrumb;
+      },
+    },
+  });
 </script>
