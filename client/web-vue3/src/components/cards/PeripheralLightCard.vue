a3e9e26f1485900934a9486249ea6f3fca276659
kirpi4ik <dumitru.ciubenco@gmail.com>
2022-02-20 18:52:38 +0200

    format

--- client/web-vue3/src/components/cards/PeripheralLightCard.vue
+++ client/web-vue3/src/components/cards/PeripheralLightCard.vue
@@ -1,135 +1,137 @@
 <template>
-  <q-card class="text-white" style="background: linear-gradient(#156cb8, #60a3c2)">
-    <q-item>
-      <q-item-section avatar>
-        <q-avatar size="60px" :class="asset['state'] ? 'shadow-10 bg-blue' : 'shadow-10 bg-yellow-4'">
-          <q-icon name="mdi-lightbulb-on" color="yellow-10" size="40px" v-if="!asset['state']"/>
-          <q-icon name="mdi-lightbulb-off" color="white" size="40px" v-if="asset['state']"/>
-        </q-avatar>
-      </q-item-section>
+	<q-card class="text-white" style="background: linear-gradient(#156cb8, #60a3c2)">
+		<q-item>
+			<q-item-section avatar>
+				<q-avatar size="60px" :class="asset['state'] ? 'shadow-10 bg-blue' : 'shadow-10 bg-yellow-4'">
+					<q-icon name="mdi-lightbulb-on" color="yellow-10" size="40px" v-if="!asset['state']" />
+					<q-icon name="mdi-lightbulb-off" color="white" size="40px" v-if="asset['state']" />
+				</q-avatar>
+			</q-item-section>
 
-      <q-item-section>
-        <q-item-label class="text-weight-medium text-h5">{{ asset.data.name }}</q-item-label>
-        <q-item-label class="text-weight-light text-blue-grey-3">{{ asset.data.description }}</q-item-label>
-        <q-item-label class="text-weight-light text-teal-2 text-caption">
-          <label v-if="config('key.on.timeout').value"
-          >[ timer: {{ humanizeDuration(Number(config('key.on.timeout').value) * 1000, { largest: 2 }) }}</label
-          >
-          <label v-if="asset.expiration" class="text-weight-light text-blue-grey-3">
-            | off at :{{ format(new Date(Number(asset.expiration)), 'HH:mm') }} </label
-          >]
-        </q-item-label>
-      </q-item-section>
+			<q-item-section>
+				<q-item-label class="text-weight-medium text-h5">{{ asset.data.name }}</q-item-label>
+				<q-item-label class="text-weight-light text-blue-grey-3">{{ asset.data.description }}</q-item-label>
+				<q-item-label class="text-weight-light text-teal-2 text-caption">
+					<label v-if="config('key.on.timeout').value"
+						>[ timer: {{ humanizeDuration(Number(config('key.on.timeout').value) * 1000, { largest: 2 }) }}</label
+					>
+					<label v-if="asset.expiration" class="text-weight-light text-blue-grey-3">
+						| off at :{{ format(new Date(Number(asset.expiration)), 'HH:mm') }} </label
+					>]
+				</q-item-label>
+			</q-item-section>
 
-      <q-item-section side>
-        <q-item-label>
-          <q-btn-dropdown size="sm" flat round icon="settings" class="text-white">
-            <q-list>
-              <q-item
-                clickable
-                v-close-popup
-                @click="setTimeout({ key: 'key.on.timeout', value: item.value, entityId: asset.id, entityType: 'PERIPHERAL' })"
-                v-for="item in stItems"
-                v-bind:key="item.value"
-              >
-                <q-item-section>
-                  <q-item-label>Timeout: {{ humanizeDuration(item.value * 1000, { largest: 2 }) }}</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup @click="deleteTimeout({ id: config('key.on.timeout').id })">
-                <q-item-section>
-                  <q-item-label>Sterge timeout</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup @click="$router.push({ path: '/admin/peripherals/' + peripheral.id + '/view' })">
-                <q-item-section>
-                  <q-item-label>Detalii</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
-        </q-item-label>
-        <q-item-label>
-          <event-logger :peripheral="peripheral"/>
-        </q-item-label>
-      </q-item-section>
-    </q-item>
+			<q-item-section side>
+				<q-item-label>
+					<q-btn-dropdown size="sm" flat round icon="settings" class="text-white">
+						<q-list>
+							<q-item
+								clickable
+								v-close-popup
+								@click="setTimeout({ key: 'key.on.timeout', value: item.value, entityId: asset.id, entityType: 'PERIPHERAL' })"
+								v-for="item in stItems"
+								v-bind:key="item.value"
+							>
+								<q-item-section>
+									<q-item-label>Timeout: {{ humanizeDuration(item.value * 1000, { largest: 2 }) }}</q-item-label>
+								</q-item-section>
+							</q-item>
+							<q-item clickable v-close-popup @click="deleteTimeout({ id: config('key.on.timeout').id })">
+								<q-item-section>
+									<q-item-label>Sterge timeout</q-item-label>
+								</q-item-section>
+							</q-item>
+							<q-item clickable v-close-popup @click="$router.push({ path: '/admin/peripherals/' + peripheral.id + '/view' })">
+								<q-item-section>
+									<q-item-label>Detalii</q-item-label>
+								</q-item-section>
+							</q-item>
+						</q-list>
+					</q-btn-dropdown>
+				</q-item-label>
+				<q-item-label>
+					<event-logger :peripheral="peripheral" />
+				</q-item-label>
+			</q-item-section>
+		</q-item>
 
-    <q-separator></q-separator>
-    <q-card-section>
-      <div class="q-pa-sm text-grey-8">
-        <toggle v-model="asset['state']"/>
-      </div>
-    </q-card-section>
-  </q-card>
+		<q-separator></q-separator>
+		<q-card-section>
+			<div class="q-pa-sm text-grey-8">
+				<toggle v-model="asset['state']" />
+			</div>
+		</q-card-section>
+	</q-card>
 </template>
 <script>
-  import {defineComponent, toRefs} from 'vue';
-  import Toggle from '@vueform/toggle';
-  import {format} from 'date-fns';
-  import humanizeDuration from 'humanize-duration';
-  import _ from 'lodash';
-  import EventLogger from 'components/EventLogger.vue';
-  import {useGlobalQueryLoading, useMutation, useQuery} from '@vue/apollo-composable';
-  import {CONFIGURATION_SET_VALUE, CONFIGURATION_DELETE, CACHE_GET_ALL_VALUES} from '@/graphql/queries';
+import { defineComponent, toRefs } from 'vue';
+import Toggle from '@vueform/toggle';
+import { format } from 'date-fns';
+import humanizeDuration from 'humanize-duration';
+import _ from 'lodash';
+import EventLogger from 'components/EventLogger.vue';
+import { useGlobalQueryLoading, useMutation, useQuery } from '@vue/apollo-composable';
+import { CONFIGURATION_SET_VALUE, CONFIGURATION_DELETE, CACHE_GET_ALL_VALUES } from '@/graphql/queries';
 
-  export default defineComponent({
-    name: 'PeripheralLightCard',
-    components: {
-      Toggle,
-      EventLogger,
-    },
-    props: {
-      peripheral: Object,
-    },
-    setup(props) {
-      const {peripheral: asset} = toRefs(props);
-      const stItems = [
-        {value: 30},
-        {value: 60},
-        {value: 300},
-        {value: 600},
-        {value: 1800},
-        {value: 3600},
-        {value: 7200},
-        {value: 10800},
-        {value: 18000},
-      ];
-      const config = key =>
-        _.find(asset.value.data.configurations, function (cfg) {
-          return cfg.key == key;
-        });
+export default defineComponent({
+	name: 'PeripheralLightCard',
+	components: {
+		Toggle,
+		EventLogger,
+	},
+	props: {
+		peripheral: Object,
+	},
+	setup(props) {
+		const { peripheral: asset } = toRefs(props);
+		const stItems = [
+			{ value: 30 },
+			{ value: 60 },
+			{ value: 300 },
+			{ value: 600 },
+			{ value: 1800 },
+			{ value: 3600 },
+			{ value: 7200 },
+			{ value: 10800 },
+			{ value: 18000 },
+		];
+		const config = key =>
+			_.find(asset.value.data.configurations, function (cfg) {
+				return cfg.key == key;
+			});
 
-      const {mutate: setTimeout} = useMutation(CONFIGURATION_SET_VALUE, {refetchQueries: [{query: CACHE_GET_ALL_VALUES, variables: {cacheName: 'expiring'}}]});
-      const {mutate: deleteTimeout} = useMutation(CONFIGURATION_DELETE);
-      return {asset, format, config, humanizeDuration, stItems, setTimeout, deleteTimeout};
-    },
-  });
+		const { mutate: setTimeout } = useMutation(CONFIGURATION_SET_VALUE, {
+			refetchQueries: [{ query: CACHE_GET_ALL_VALUES, variables: { cacheName: 'expiring' } }],
+		});
+		const { mutate: deleteTimeout } = useMutation(CONFIGURATION_DELETE);
+		return { asset, format, config, humanizeDuration, stItems, setTimeout, deleteTimeout };
+	},
+});
 </script>
 <style>
-  .toggle,
-  .toggle-container {
-    width: 100% !important;
-    height: 40px !important;
-  }
+.toggle,
+.toggle-container {
+	width: 100% !important;
+	height: 40px !important;
+}
 
-  .toggle-handle {
-    height: 40px !important;
-    width: 40px !important;
-  }
+.toggle-handle {
+	height: 40px !important;
+	width: 40px !important;
+}
 
-  .toggle-off {
-    background-color: red !important;
-  }
+.toggle-off {
+	background-color: red !important;
+}
 
-  .toggle-on .toggle-handle {
-    background: linear-gradient(rgb(0, 191, 54), rgb(53, 113, 61)) !important;
-    border: 1px solid #386668;
-  }
+.toggle-on .toggle-handle {
+	background: linear-gradient(rgb(0, 191, 54), rgb(53, 113, 61)) !important;
+	border: 1px solid #386668;
+}
 
-  .toggle-off .toggle-handle {
-    background: linear-gradient(rgb(191, 0, 0), rgb(255, 190, 98)) !important;
-    border: 1px solid #386668;
-  }
+.toggle-off .toggle-handle {
+	background: linear-gradient(rgb(191, 0, 0), rgb(255, 190, 98)) !important;
+	border: 1px solid #386668;
+}
 </style>
 <style src="@vueform/toggle/themes/default.css"></style>
--- client/web-vue3/src/pages/ZoneCombinedView.vue
+++ client/web-vue3/src/pages/ZoneCombinedView.vue
@@ -1,134 +1,131 @@
 <template>
-  <q-page class="q-pa-sm">
-    <div class="row q-col-gutter-lg">
-      <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" v-for="zone in childZones" v-bind:key="zone.id">
-        <zone-card :zone="zone"/>
-      </div>
+	<q-page class="q-pa-sm">
+		<div class="row q-col-gutter-lg">
+			<div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" v-for="zone in childZones" v-bind:key="zone.id">
+				<zone-card :zone="zone" />
+			</div>
 
-      <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" v-for="peripheral in peripheralList" v-bind:key="peripheral.id">
-        <peripheral-light-card :peripheral="peripheral" v-if="category === 'LIGHT'"/>
-        <peripheral-heat-card :peripheral="peripheral" v-if="category === 'HEAT'"/>
-      </div>
-    </div>
-  </q-page>
+			<div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" v-for="peripheral in peripheralList" v-bind:key="peripheral.id">
+				<peripheral-light-card :peripheral="peripheral" v-if="category === 'LIGHT'" />
+				<peripheral-heat-card :peripheral="peripheral" v-if="category === 'HEAT'" />
+			</div>
+		</div>
+	</q-page>
 </template>
 
 <script>
-  import ZoneCard from '@/components/cards/ZoneCard';
-  import PeripheralLightCard from '@/components/cards/PeripheralLightCard';
-  import PeripheralHeatCard from '@/components/cards/PeripheralHeatCard';
-  import {ZONE_GET_BY_ID, ZONES_GET_ROOT, CACHE_GET_ALL_VALUES} from '@/graphql/queries';
-  import {useGlobalQueryLoading, useQuery} from '@vue/apollo-composable';
-  import {useRoute} from 'vue-router';
-  import _ from 'lodash';
-  import {ref} from 'vue';
+import ZoneCard from '@/components/cards/ZoneCard';
+import PeripheralLightCard from '@/components/cards/PeripheralLightCard';
+import PeripheralHeatCard from '@/components/cards/PeripheralHeatCard';
+import { ZONE_GET_BY_ID, ZONES_GET_ROOT, CACHE_GET_ALL_VALUES } from '@/graphql/queries';
+import { useGlobalQueryLoading, useQuery } from '@vue/apollo-composable';
+import { useRoute } from 'vue-router';
+import _ from 'lodash';
+import { ref } from 'vue';
 
-  export default {
-    components: {
-      ZoneCard,
-      PeripheralLightCard,
-      PeripheralHeatCard,
-    },
-    setup() {
-      const route = useRoute();
-      const category = route.query.category;
-      let currentZone = {};
-      let childZones = ref([]);
-      let peripheralList = ref([]);
-      let assetExpirationMap = ref({});
+export default {
+	components: {
+		ZoneCard,
+		PeripheralLightCard,
+		PeripheralHeatCard,
+	},
+	setup() {
+		const route = useRoute();
+		const category = route.query.category;
+		let currentZone = {};
+		let childZones = ref([]);
+		let peripheralList = ref([]);
+		let assetExpirationMap = ref({});
 
-      const {onResult: onCacheResult} = useQuery(
-        CACHE_GET_ALL_VALUES,
-        {cacheName: 'expiring'}
-      );
-      onCacheResult(queryResult => {
-        assetExpirationMap.value = _.reduce(
-          queryResult.data.cacheAll,
-          function (hash, value) {
-            var key = value['cacheKey'];
-            hash[key] = value['cachedValue'];
-            return hash;
-          },
-          {},
-        );
-      });
+		const { onResult: onCacheResult } = useQuery(CACHE_GET_ALL_VALUES, { cacheName: 'expiring' });
+		onCacheResult(queryResult => {
+			assetExpirationMap.value = _.reduce(
+				queryResult.data.cacheAll,
+				function (hash, value) {
+					var key = value['cacheKey'];
+					hash[key] = value['cachedValue'];
+					return hash;
+				},
+				{},
+			);
+		});
 
-      const peripheralFilter = peripheral => {
-        return peripheral.category.name === category;
-      };
-      const peripheralInitCallback = peripheral => {
-        if (peripheral != null) {
-          if (peripheral.connectedTo && peripheral.connectedTo.length > 0) {
-            let port = peripheral.connectedTo[0];
-            if (port != null && port.device != null) {
-              peripheral['value'] = peripheral.connectedTo[0].value;
-              peripheral['state'] = peripheral.connectedTo[0].value === 'OFF';
-              peripheral['deviceState'] = peripheral.connectedTo[0].device.status;
-            } else {
-              peripheral['deviceState'] = 'OFFLINE';
-            }
-          }
-          let asset = {
-            data: peripheral,
-            id: peripheral['id'],
-            value: peripheral['value'],
-            state: peripheral['state'],
-            deviceState: peripheral['deviceState'],
-            expiration: assetExpirationMap.value[peripheral.connectedTo[0].id],
-          };
-          peripheralList.value.push(asset);
-        }
-      };
+		const peripheralFilter = peripheral => {
+			return peripheral.category.name === category;
+		};
+		const peripheralInitCallback = peripheral => {
+			if (peripheral != null) {
+				if (peripheral.connectedTo && peripheral.connectedTo.length > 0) {
+					let port = peripheral.connectedTo[0];
+					if (port != null && port.device != null) {
+						peripheral['value'] = peripheral.connectedTo[0].value;
+						peripheral['state'] = peripheral.connectedTo[0].value === 'OFF';
+						peripheral['deviceState'] = peripheral.connectedTo[0].device.status;
+					} else {
+						peripheral['deviceState'] = 'OFFLINE';
+					}
+				}
+				let asset = {
+					data: peripheral,
+					id: peripheral['id'],
+					value: peripheral['value'],
+					state: peripheral['state'],
+					deviceState: peripheral['deviceState'],
+					expiration: assetExpirationMap.value[peripheral.connectedTo[0].id],
+				};
+				peripheralList.value.push(asset);
+			}
+		};
 
-      let localPList;
-      if (route.params.zoneId) {
-        const {onResult: onResultById} = useQuery(
-          ZONE_GET_BY_ID,
-          {id: route.params.zoneId},
-          {
-            fetchPolicy: 'network-only', // Used for first execution
-            nextFetchPolicy: 'cache-and-network', // Used for subsequent executions
-          },
-        );
-        onResultById(queryResult => {
-          let data = _.cloneDeep(queryResult.data);
-          peripheralList.value = [];
-          currentZone = data.zoneById;
+		let localPList;
+		if (route.params.zoneId) {
+			const { onResult: onResultById } = useQuery(
+				ZONE_GET_BY_ID,
+				{ id: route.params.zoneId },
+				{
+					fetchPolicy: 'network-only', // Used for first execution
+					nextFetchPolicy: 'cache-and-network', // Used for subsequent executions
+				},
+			);
+			onResultById(queryResult => {
+				let data = _.cloneDeep(queryResult.data);
+				peripheralList.value = [];
+				currentZone = data.zoneById;
 
-          if (currentZone.peripherals) {
-            localPList = currentZone.peripherals.filter(peripheralFilter);
-            localPList.sort((a, b) => (a.name > b.name ? 1 : -1));
-            localPList.forEach(peripheralInitCallback);
-          }
-          childZones.value = currentZone.zones;
-          // [...childZones].sort((a, b) => (a.name > b.name) ? 1 : -1)
-        });
-      }
-      if (route.params.zoneId == null) {
-        peripheralList.value = [];
-        const {onResult: onResultRoot} = useQuery(ZONES_GET_ROOT);
-        onResultRoot(queryResult => {
-          let data = _.cloneDeep(queryResult.data);
+				if (currentZone.peripherals) {
+					localPList = currentZone.peripherals.filter(peripheralFilter);
+					localPList.sort((a, b) => (a.name > b.name ? 1 : -1));
+					localPList.forEach(peripheralInitCallback);
+				}
+				childZones.value = currentZone.zones;
+				// [...childZones].sort((a, b) => (a.name > b.name) ? 1 : -1)
+			});
+		}
+		if (route.params.zoneId == null) {
+			peripheralList.value = [];
+			const { onResult: onResultRoot } = useQuery(ZONES_GET_ROOT);
+			onResultRoot(queryResult => {
+				let data = _.cloneDeep(queryResult.data);
 
-          currentZones = [];
-          if (data.zonesRoot.peripherals) {
-            localPList = data.zonesRoot.peripherals.filter(peripheralFilter);
-            localPList.sort((a, b) => (a.name > b.name ? 1 : -1));
-            localPList.forEach(peripheralInitCallback);
-          }
-          childZones = data.zonesRoot.zones;
-          // [...childZones].sort((a, b) => (a.name > b.name) ? 1 : -1)
-        });
-      }
+				currentZones = [];
+				if (data.zonesRoot.peripherals) {
+					localPList = data.zonesRoot.peripherals.filter(peripheralFilter);
+					localPList.sort((a, b) => (a.name > b.name ? 1 : -1));
+					localPList.forEach(peripheralInitCallback);
+				}
+				childZones = data.zonesRoot.zones;
+				// [...childZones].sort((a, b) => (a.name > b.name) ? 1 : -1)
+			});
+		}
 
-      const loading = useGlobalQueryLoading();
-      return {
-        loading,
-        childZones,
-        peripheralList,
-        category,
-        assetExpirationMap,
-      };
-    },
-  };
+		const loading = useGlobalQueryLoading();
+		return {
+			loading,
+			childZones,
+			peripheralList,
+			category,
+			assetExpirationMap,
+		};
+	},
+};
 </script>
