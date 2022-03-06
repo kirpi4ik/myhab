eb73fd200587f640b4b61561231c2f4ccbcb1746
kirpi4ik <dumitru.ciubenco@gmail.com>
2022-03-06 21:54:18 +0200

    add hasRole check

--- client/web-vue3/src/_services/authentication.service.js
+++ client/web-vue3/src/_services/authentication.service.js
@@ -1,32 +1,39 @@
-import { BehaviorSubject } from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 
-import { handleResponse, requestOptions, Utils } from '@/_helpers';
+import {handleResponse, requestOptions, Utils} from '@/_helpers';
 
 const currentUserSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('currentUser')));
 
 export const authenticationService = {
-	login,
-	logout,
-	currentUser: currentUserSubject.asObservable(),
-	get currentUserValue() {
-		return currentUserSubject.value;
-	},
+  login,
+  logout,
+  currentUser: currentUserSubject.asObservable(),
+  get currentUserValue() {
+    return currentUserSubject.value;
+  },
+  hasAnyRole
 };
 
 function login(username, password) {
-	return fetch(`${Utils.host()}/api/login`, requestOptions.post({ username, password }))
-		.then(handleResponse)
-		.then(user => {
-			// store user details and jwt token in local storage to keep user logged in between page refreshes
-			localStorage.setItem('currentUser', JSON.stringify(user));
-			currentUserSubject.next(user);
-			return user;
-		});
+  return fetch(`${Utils.host()}/api/login`, requestOptions.post({username, password}))
+    .then(handleResponse)
+    .then(user => {
+      // store user details and jwt token in local storage to keep user logged in between page refreshes
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      currentUserSubject.next(user);
+      return user;
+    });
 }
 
 function logout() {
-	// remove user from local storage to log user out
-	localStorage.removeItem('currentUser');
-	currentUserSubject.next(null);
-	location.replace('/login');
+  // remove user from local storage to log user out
+  localStorage.removeItem('currentUser');
+  currentUserSubject.next(null);
+  location.replace('/login');
+}
+
+function hasAnyRole(roles) {
+  return currentUserSubject.value.permissions.filter(function (userRole) {
+    return roles.includes(userRole);
+  }).length > 0;
 }
