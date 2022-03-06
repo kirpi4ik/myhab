d2d03fa575ba87a0f9aa987a5f34f00b88a4904c
kirpi4ik <dumitru.ciubenco@gmail.com>
2022-03-06 20:16:00 +0200

    toggle styling

--- client/web-vue3/src/components/cards/PeripheralHeatCard.vue
+++ client/web-vue3/src/components/cards/PeripheralHeatCard.vue
@@ -148,30 +148,4 @@ export default defineComponent({
 	},
 });
 </script>
-<style>
-.toggle,
-.toggle-container {
-	width: 100% !important;
-	height: 40px !important;
-}
-
-.toggle-handle {
-	height: 40px !important;
-	width: 40px !important;
-}
-
-.toggle-off {
-	background-color: red !important;
-}
-
-.toggle-on .toggle-handle {
-	background: linear-gradient(rgb(0, 191, 54), rgb(53, 113, 61)) !important;
-	border: 1px solid #386668;
-}
-
-.toggle-off .toggle-handle {
-	background: linear-gradient(rgb(191, 0, 0), rgb(255, 190, 98)) !important;
-	border: 1px solid #386668;
-}
-</style>
 <style src="@vueform/toggle/themes/default.css"></style>
