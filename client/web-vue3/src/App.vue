ebb103ad83e0475def2a5680b2a026b579a9a6e4
kirpi4ik <dumitru.ciubenco@gmail.com>
2022-03-08 09:56:24 +0200

    yarn upgrade add nav animation replace home btn

--- client/web-vue3/src/App.vue
+++ client/web-vue3/src/App.vue
@@ -1,17 +1,24 @@
 <template>
-	<router-view :key="$route.fullPath" />
+  <transition
+    appear
+    enter-active-class="animated fadeIn"
+    leave-active-class="animated fadeOut"
+    mode="out-in"
+    :duration="200">
+    <router-view :key="$route.fullPath"/>
+  </transition>
 </template>
 <script>
-import { defineComponent } from 'vue';
-import { mapActions } from 'vuex';
+  import {defineComponent} from 'vue';
+  import {mapActions} from 'vuex';
 
-export default defineComponent({
-	name: 'App',
-	methods: {
-		...mapActions(['connect']),
-	},
-	mounted() {
-		this.connect();
-	},
-});
+  export default defineComponent({
+    name: 'App',
+    methods: {
+      ...mapActions(['connect']),
+    },
+    mounted() {
+      this.connect();
+    },
+  });
 </script>
