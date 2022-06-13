2db531b2d37665e3913f5952742070d6079e6388
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2022-06-13 17:07:55 +0400

    Admin:  - user list disaply  - user creation/deleteion

--- client/web-vue3/src/pages/infra/user/UserView.vue
+++ client/web-vue3/src/pages/infra/user/UserView.vue
@@ -1,9 +1,26 @@
 <template>
-	<q-page padding> y </q-page>
+  <q-page padding>
+    <q-card class="my-card">
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Title</q-item-label>
+          <q-item-label caption>Subhead</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+    </q-card>
+  </q-page>
 </template>
 
 <script>
 export default {
-	// name: 'PageName',
+  // name: 'PageName',
 };
 </script>
