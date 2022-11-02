fd686676b80eeddecfb7ea1d1e6e1d58a9a1a266
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2022-11-02 10:58:06 +0200

    Peripheral categories

---
+++ client/web-vue3/src/pages/infra/peripheral/categories/CategoryView.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-page padding>
+    <q-card class="my-card" v-if="viewItem">
+      <q-card-section>
+        <q-avatar size="103px" class="absolute-center shadow-10">
+          <q-icon name="mdi-shape"/>
+        </q-avatar>
+        <q-btn flat color="secondary" @click="$router.go(-1)" align="right" label="Back" icon="mdi-arrow-left"/>
+      </q-card-section>
+      <q-card-section>
+        <div class="text-h4 text-secondary">Category details</div>
+      </q-card-section>
+      <q-item>
+        <q-item-section>
+          <q-item-label>Name</q-item-label>
+          <q-item-label caption>{{ viewItem.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section>
+          <q-item-label>Description</q-item-label>
+          <q-item-label caption>{{ viewItem.description }}</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-card-actions>
+        <q-btn flat color="secondary" :to="uri +'/'+ $route.params.idPrimary+'/edit'">
+          Edit
+        </q-btn>
+        <q-btn flat color="secondary" :to="$route.matched[$route.matched.length-2]">
+          Cancel
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import {defineComponent, onMounted, ref} from "vue";
+import {PERIPHERAL_CATEGORY_BY_ID} from "@/graphql/queries";
+import {useApolloClient} from "@vue/apollo-composable";
+import {useRoute, useRouter} from "vue-router/dist/vue-router";
+
+export default defineComponent({
+  name: 'PCategoryView',
+  setup() {
+    const uri = '/admin/pcategories'
+    const viewItem = ref()
+    const loading = ref(false)
+    const {client} = useApolloClient();
+    const router = useRouter();
+
+    const fetchData = () => {
+      loading.value = true;
+      client.query({
+        query: PERIPHERAL_CATEGORY_BY_ID,
+        variables: {id: useRoute().params.idPrimary},
+        fetchPolicy: 'network-only',
+      }).then(response => {
+        viewItem.value = response.data.peripheralCategory
+        loading.value = false;
+      });
+    }
+    onMounted(() => {
+      fetchData()
+    })
+    return {
+      uri,
+      fetchData,
+      viewItem
+    }
+  }
+});
+</script>
