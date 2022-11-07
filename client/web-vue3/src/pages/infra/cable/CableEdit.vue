85a0bcc60041d5bfa577384027ee8c4248832f7d
Dumitru Ciubenco <dumitru.ciubenco@gmail.com>
2022-11-07 22:42:48 +0200

    Cable editor, connect ports

--- client/web-vue3/src/pages/infra/cable/CableEdit.vue
+++ client/web-vue3/src/pages/infra/cable/CableEdit.vue
@@ -10,6 +10,12 @@
                    :rules="[val => !!val || 'Field is required']"/>
           <q-input v-model="cable.nrWires" label="Nr wires" clearable clear-icon="close" color="green"/>
           <q-input v-model="cable.maxAmp" label="Max amp" clearable clear-icon="close" color="green"/>
+          <q-select v-model="cable.category"
+                    :options="cableCategoryList"
+                    option-label="name"
+                    label="Category" map-options filled dense color="green">
+            <q-icon name="cancel" @click.stop.prevent="cable.category = null" class="cursor-pointer text-blue"/>
+          </q-select>
           <q-select v-model="cable.rack"
                     :options="rackList"
                     option-label="name"
@@ -29,6 +35,38 @@
           </q-card-section>
           <q-input v-model="cable.codeOld" label="Code old" clearable clear-icon="close" color="green"/>
           <q-input v-model="cable.codeNew" label="Code new" clearable clear-icon="close" color="green"/>
+        </q-card-section>
+        <q-card-section>
+          <q-item-label>Port connect</q-item-label>
+          <div class="row q-col-gutter-xs">
+            <q-select v-model="newPortDevice"
+                      :options="deviceList"
+                      option-label="name"
+                      label="Device" map-options filled dense color="green" class="col-lg-2 col-md-2"
+                      @update:model-value="selectDevice">
+              <q-icon name="cancel" @click.stop.prevent="newPortDevice = null" class="cursor-pointer text-blue"/>
+            </q-select>
+            <q-select v-if="newPortDevice != null"
+                      v-model="newPort"
+                      :options="newPortDevice.ports"
+                      option-label="name"
+                      label="Port" map-options filled dense color="green" class="col-lg-2 col-md-2">
+              <q-icon name="cancel" class="cursor-pointer text-blue"
+              />
+            </q-select>
+            <q-btn icon="mdi-link-variant-plus" @click="connectPort()" color="green" label="Connect"/>
+          </div>
+          <div class="row">
+            <q-table
+              class="col"
+              title="Connected to ports"
+              :rows="cable.connectedTo"
+              :columns="portColumns"
+              @row-click="viewPort"
+              row-key="id"
+            >
+            </q-table>
+          </div>
         </q-card-section>
         <q-separator/>
         <q-card-actions>
@@ -66,8 +104,18 @@ export default defineComponent({
     const cable = ref({})
     const rackList = ref([])
     const patchPanelList = ref([])
+    const cableCategoryList = ref([])
+    const deviceList = ref([])
     const router = useRouter();
     const route = useRoute();
+    const newPortDevice = ref(null)
+    const newPort = ref()
+
+    const portColumns = [
+      {name: 'id', label: 'ID', field: 'id', align: 'left', sortable: true},
+      {name: 'name', label: 'Name', field: 'name', align: 'left', sortable: true},
+      {name: 'internalRef', label: 'Int Ref', field: 'internalRef', align: 'left', sortable: true}
+    ]
 
     const fetchData = () => {
       client.query({
@@ -78,6 +126,8 @@ export default defineComponent({
         cable.value = _.cloneDeep(response.data.cable)
         patchPanelList.value = response.data.patchPanelList
         rackList.value = response.data.rackList
+        cableCategoryList.value = response.data.cableCategoryList
+        deviceList.value = response.data.deviceList
       })
     }
     const onSave = () => {
@@ -88,6 +138,10 @@ export default defineComponent({
         })
       } else {
         delete cable.value.id
+        cable.value.connectedTo.forEach(function (port) {
+          delete port.device
+        })
+
         client.mutate({
           mutation: CABLE_VALUE_UPDATE,
           variables: {id: route.params.idPrimary, cable: cable.value},
@@ -103,7 +157,23 @@ export default defineComponent({
       cable,
       onSave,
       rackList,
-      patchPanelList
+      cableCategoryList,
+      patchPanelList,
+      portColumns,
+      deviceList,
+      newPort,
+      newPortDevice,
+      viewPort: (evt, row) => {
+        if (evt.target.nodeName === 'TD' || evt.target.nodeName === 'DIV') {
+          router.push({path: `/admin/ports/${row.id}/view`})
+        }
+      },
+      selectDevice: (evt) => {
+        newPort.value = null
+      },
+      connectPort: (evt) => {
+        cable.value.connectedTo.push(newPort.value)
+      }
     }
   }
 });
