885b41db6d2d48c1dfe8c6805c7e80c94ccab196
kirpi4ik <dumitru.ciubenco@gmail.com>
2023-11-14 17:03:04 +0200

    Add peripheral edit UI

--- client/web-vue3/src/pages/infra/peripheral/PeripheralEdit.vue
+++ client/web-vue3/src/pages/infra/peripheral/PeripheralEdit.vue
@@ -1,9 +1,158 @@
 <template>
-	<q-page padding> z </q-page>
+  <q-page padding>
+    <form @submit.prevent.stop="onSave" class="q-gutter-md">
+      <q-card flat bordered>
+        <q-card-section class="full-width">
+          <div class="text-h5">Editare : {{ peripheral.name }}</div>
+          <q-select v-model="peripheral.connectedTo"
+                    :options="portListFiltered"
+                    :disable="portListDisabled"
+                    input-debounce="0"
+                    @filter="searchPortFn"
+                    :option-label="opt => opt.name+(opt.device != null?'['+opt.device.code+']':'')"
+                    map-options
+                    stack-label
+                    use-chips
+                    use-input
+                    filled
+                    dense
+                    multiple
+                    label="Port">
+            <q-icon name="cancel" @click.stop.prevent="peripheral.connectedTo = null" class="cursor-pointer text-blue"/>
+          </q-select>
+          <q-input v-model="peripheral.name" label="Name" clearable clear-icon="close" color="orange"
+                   :rules="[val => !!val || 'Field is required']"/>
+          <q-input v-model="peripheral.model" label="Model" clearable clear-icon="close" color="orange"/>
+          <q-input v-model="peripheral.description" label="Description" clearable clear-icon="close" color="orange"
+                   :rules="[val => !!val || 'Field is required']"/>
+          <q-select v-model="peripheral.category"
+                    :options="categoryList"
+                    option-label="name"
+                    label="Category" map-options filled dense>
+            <q-icon name="cancel" @click.stop.prevent="peripheral.category = null" class="cursor-pointer text-blue"/>
+          </q-select>
+        </q-card-section>
+        <q-separator/>
+        <q-card-actions>
+          <q-btn flat color="secondary" type="submit">
+            Save
+          </q-btn>
+          <q-btn flat color="secondary" :to="$route.matched[1]">
+            Cancel
+          </q-btn>
+        </q-card-actions>
+      </q-card>
+    </form>
+  </q-page>
 </template>
 
 <script>
-export default {
-	// name: 'PageName',
-};
+import {useQuasar} from 'quasar'
+import {defineComponent, onMounted, ref} from 'vue';
+import {PERIPHERAL_CATEGORIES, PERIPHERAL_GET_BY_ID, PERIPHERAL_UPDATE, PORT_LIST_ALL} from '@/graphql/queries';
+import {useApolloClient} from "@vue/apollo-composable";
+import {useRoute, useRouter} from "vue-router/dist/vue-router";
+import _ from "lodash";
+
+export default defineComponent({
+    name: 'PeripheralEdit',
+    setup() {
+      const $q = useQuasar()
+      const {client} = useApolloClient();
+      const peripheral = ref({connectedTo: []})
+      const categoryList = ref([])
+      const router = useRouter();
+      const route = useRoute();
+      const portList = ref([])
+      const portListFiltered = ref([])
+      const portListDisabled = ref(false)
+      const loading = ref(false)
+
+      const fetchData = () => {
+        loading.value = true;
+        client.query({
+          query: PERIPHERAL_GET_BY_ID,
+          variables: {id: useRoute().params.idPrimary},
+          fetchPolicy: 'network-only',
+        }).then(response => {
+          peripheral.value = response.data.devicePeripheral
+          loading.value = false;
+        });
+        client.query({
+          query: PERIPHERAL_CATEGORIES,
+          variables: {},
+          fetchPolicy: 'network-only',
+        }).then(response => {
+          categoryList.value = response.data.peripheralCategoryList
+        })
+
+        client.query({
+          query: PORT_LIST_ALL,
+          variables: {},
+          fetchPolicy: 'network-only',
+        }).then(response => {
+          portList.value = _.transform(response.data.devicePortList,
+            function (result, value, key) {
+              let port = {
+                id: value.id,
+                name: value.name + '[' + value.device.code + ']'
+              }
+              result.push(port)
+            });
+          if (route.query.portId != null) {
+            peripheral.value.connectedTo = _.find(portList.value, function (o) {
+              return o.id == route.query.portId;
+            });
+            portListDisabled.value = true
+          }
+          portListFiltered.value = [...portList.value]
+        })
+      }
+      const searchPortFn = (val, update) => {
+
+        update(() => {
+          const needle = val.toLowerCase()
+          portListFiltered.value = portList.value.filter(option => {
+            return option.name.toLowerCase().indexOf(needle) > -1
+          })
+        })
+      }
+
+      const onSave = () => {
+        if (peripheral.value.hasError) {
+          $q.notify({
+            color: 'negative',
+            message: 'Failed submission'
+          })
+        } else {
+          delete peripheral.value.id
+          delete peripheral.value.configurations
+          peripheral.value.connectedTo.forEach(function (port) {
+            delete port.device
+          })
+
+          client.mutate({
+            mutation: PERIPHERAL_UPDATE,
+            variables: {id: route.params.idPrimary, devicePeripheral: peripheral.value},
+          }).then(response => {
+            router.push({path: `/admin/peripherals/${route.params.idPrimary}/edit`})
+          });
+        }
+      }
+      onMounted(() => {
+        fetchData()
+      })
+      return {
+        peripheral,
+        categoryList,
+        portList,
+        portListFiltered,
+        portListDisabled,
+        onSave,
+        searchPortFn
+      }
+    }
+  }
+)
+;
 </script>
