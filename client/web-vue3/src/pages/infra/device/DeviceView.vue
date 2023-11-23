dca732f276460cdb5b01e65e134ec60015e7a8ea
kirpi4ik <dumitru.ciubenco@gmail.com>
2023-11-23 15:32:18 +0200

    use same action button group for all lists

--- client/web-vue3/src/pages/infra/device/DeviceView.vue
+++ client/web-vue3/src/pages/infra/device/DeviceView.vue
@@ -8,10 +8,11 @@
         <q-btn flat color="secondary" @click="$router.go(-1)" align="right" label="Back" icon="mdi-arrow-left"/>
       </q-card-section>
       <q-card-section class="row">
-        <div class="text-h4 text-secondary">Device details: {{viewItem.code}}</div>
+        <div class="text-h4 text-secondary">Device details: {{ viewItem.code }}</div>
         <q-btn outline round color="amber-8" icon="mdi-pencil" :to="uri +'/'+ $route.params.idPrimary+'/edit'"
                style="margin-left: 5px"/>
-        <q-btn outline round color="amber-8" icon="mdi-view-list" :to="'/admin/configurations/'+ $route.params.idPrimary+'?type=DEVICE'"
+        <q-btn outline round color="amber-8" icon="mdi-view-list"
+               :to="'/admin/configurations/'+ $route.params.idPrimary+'?type=DEVICE'"
                style="margin-left: 5px"/>
       </q-card-section>
       <q-item>
@@ -54,12 +55,12 @@
         >
           <template v-slot:body-cell-actions="props">
             <q-td :props="props">
-              <q-btn icon="delete"  @click="confirmDelete = true; selectedPort=props.row"></q-btn>
+              <q-btn icon="delete" @click.stop="removeItem(props.row)"/>
             </q-td>
           </template>
         </q-table>
         <br/>
-        <q-btn icon="add" color="positive"  label="Add port" @click="newPort"/>
+        <q-btn icon="add" color="positive" label="Add port" @click="newPort"/>
 
       </div>
 
@@ -72,49 +73,26 @@
         </q-btn>
       </q-card-actions>
     </q-card>
-    <q-dialog v-model="confirmDelete" transition-show="jump-up" transition-hide="jump-down">
-      <q-card>
-        <q-bar class="bg-deep-orange-7 text-white">
-          <q-icon name="lock"/>
-          <div>Warning</div>
-          <q-space/>
-          <q-btn dense flat icon="close" v-close-popup>
-            <q-tooltip class="bg-primary">Close</q-tooltip>
-          </q-btn>
-        </q-bar>
-        <q-card-section class="row items-center">
-          <q-avatar icon="delete" color="red" text-color="white"/>
-          <span class="q-ml-sm">Do you want to remove port  <b class="text-h6 text-red">{{ selectedPort.name }}</b> ?</span>
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat label="Delete" color="red" v-close-popup
-                 @click="onDelete();confirmDelete=false"/>
-          <q-btn flat label="Cancel" color="positive" v-close-popup/>
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
   </q-page>
 </template>
 
 <script>
 import {defineComponent, onMounted, ref} from "vue";
-import {DEVICE_GET_BY_ID_CHILDS, PORT_DELETE_BY_ID} from "@/graphql/queries";
+import {CONFIGURATION_REMOVE_CONFIG, DEVICE_GET_BY_ID_CHILDS, PORT_DELETE_BY_ID} from "@/graphql/queries";
 import {useApolloClient} from "@vue/apollo-composable";
 import {useRoute, useRouter} from "vue-router/dist/vue-router";
+import {useQuasar} from "quasar";
 
 export default defineComponent({
   name: 'DeviceView',
   setup() {
     const uri = '/admin/devices'
+    const $q = useQuasar()
     const viewItem = ref()
     const loading = ref(false)
     const {client} = useApolloClient();
     const router = useRouter();
     const route = useRoute();
-    const confirmDelete = ref(false);
-    const selectedPort = ref(null);
-
 
     const portColumns = [
       {name: 'id', label: 'ID', field: 'id', align: 'left', sortable: true},
@@ -136,6 +114,39 @@ export default defineComponent({
         loading.value = false;
       });
     }
+    const removeItem = (toDelete) => {
+      $q.dialog({
+        title: 'Remove',
+        message: `Doriti sa stergeti portul : ${toDelete.name} ?`,
+        ok: {
+          push: true,
+          color: 'negative',
+          label: "Remove"
+        },
+        cancel: {
+          push: true,
+          color: 'green'
+        }
+      }).onOk(() => {
+        client.mutate({
+          mutation: PORT_DELETE_BY_ID,
+          variables: {id: toDelete.id},
+        }).then(response => {
+          fetchData()
+          if (response.data.devicePortDelete.success) {
+            $q.notify({
+              color: 'positive',
+              message: 'Port removed'
+            })
+          } else {
+            $q.notify({
+              color: 'negative',
+              message: 'Failed to remove'
+            })
+          }
+        });
+      })
+    }
     onMounted(() => {
       fetchData()
     })
@@ -143,6 +154,7 @@ export default defineComponent({
       uri,
       fetchData,
       viewItem,
+      removeItem,
       portColumns,
       onRowClick: (evt, row) => {
         if (evt.target.nodeName === 'TD' || evt.target.nodeName === 'DIV') {
@@ -151,16 +163,6 @@ export default defineComponent({
       },
       newPort: () => {
         router.push({path: `/admin/ports/new`, query: {deviceId: route.params.idPrimary}})
-      },
-      confirmDelete,
-      selectedPort,
-      onDelete: () => {
-        client.mutate({
-          mutation: PORT_DELETE_BY_ID,
-          variables: {id: selectedPort.value.id},
-        }).then(response => {
-          fetchData()
-        });
       }
     }
   }
