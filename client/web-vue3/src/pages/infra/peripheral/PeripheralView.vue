dde8f91a2a030df6b2ece2ff86b03d50bab7e911
kirpi4ik <dumitru.ciubenco@gmail.com>
2023-11-23 15:23:08 +0200

    use same action button group for all lists

--- client/web-vue3/src/pages/infra/peripheral/PeripheralView.vue
+++ client/web-vue3/src/pages/infra/peripheral/PeripheralView.vue
@@ -13,8 +13,6 @@
                style="margin-left: 5px"/>
         <q-btn outline round color="amber-8" icon="mdi-view-list" :to="'/admin/configurations/'+ $route.params.idPrimary+'?type=PERIPHERAL'"
                style="margin-left: 5px"/>
-        <q-btn outline round color="amber-8" icon="mdi-delete-outline" :to="'/admin/configurations/'+ $route.params.idPrimary+'?type=PERIPHERAL'"
-               style="margin-left: 5px"/>
       </q-card-section>
       <q-item>
         <q-item-section>
